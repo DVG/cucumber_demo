6d22994c5f8bf101d69c94ccc9b8f80f2114ea94
Bradley Temple <bradley.temple@jpmchase.com>
2013-03-12 15:52:37 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '1c90ef6f7eddcd75978730976cb85beb91cd631aa6ccc6f73186d6a530ee8f4a74c0423386c97e557bb8f4dfdfcf64ea4defbc8209f0cbf0e8252065dfa55e68'
